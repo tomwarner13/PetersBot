3dff35442197a3762213d058ebdbbeb65879a563
Tom Warner <twarner@dealeron.com>
2017-02-22 18:55:06 -0500

    stubbed out some API stuff

--- peters.js
+++ peters.js
@@ -5,7 +5,34 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var app = express();
 
+//user management
+//TODO: figure out if we can integrate w/ hipchat API for user mgmt
 
+app.get('/users', (req, res) => {
+	//return user list
+})
+
+app.get('/users/{id}', (req, res) => {
+	//return user with specific ID
+})
+
+//order management
+
+app.post('/orders', (req, res) => {
+	//create new order, return order ID
+})
+
+app.patch('/orders/{orderId}', (req, res) => {
+	//edit order with specific ID
+})
+
+app.get('/orders', (req, res) => {
+	//return all orders
+})
+
+app.get('/orders/{id}', (req, res) => {
+	//return order with specific ID
+})
 
 app.use(express.static('main'));
 
